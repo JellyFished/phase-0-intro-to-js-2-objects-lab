352bec3cde565c657e56a85da3f7a9cb3bb09792
JellyFished <gisele0kms@gmail.com>
2024-02-11 15:09:03 -0500

    employee name and address change

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+//employee.object;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+ 
+employee.streetAddress = "12 Broadway";
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+
+    newEmployee['name'] = key;
+
+    delete newEmployee.name;
+
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    employee['name'] = key;
+
+    delete employee.name;
+
+    return employee;
+
+}
+
+
